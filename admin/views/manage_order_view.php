ef1030249edcb15d282cff2ccd5a5c38b2bf0ddc
smabdulmueid97 <smabdulmueid97@gmail.com~>
2024-06-04 20:27:39 +0800

    init

--- admin/views/manage_order_view.php
+++ admin/views/manage_order_view.php
@@ -5,22 +5,49 @@
 $catagories = $obj->display_catagory();
 $all_order_info = $obj->all_order_info();
 
+// Initialize the order_infos array
 $order_infos = array();
-while($all_order = mysqli_fetch_assoc($all_order_info)){
-    $order_infos[] =$all_order; 
+while ($all_order = mysqli_fetch_assoc($all_order_info)) {
+    $order_infos[] = $all_order;
 }
 
-if(isset($_POST['update_status_btn'])){
-   
-   $status_msg =  $obj->updat_order_status($_POST);
+// Handle the search queries
+if (isset($_POST['search_mobile_btn'])) {
+    $search_query = $_POST['search_query'];
+    $order_infos = array_filter($order_infos, function ($order) use ($search_query) {
+        return stripos($order['Shipping_mobile'], $search_query) !== false;
+    });
 }
 
-if(isset( $status_msg)){
-    echo  $status_msg ;
+if (isset($_POST['search_order_id_btn'])) {
+    $search_order_id = $_POST['search_order_id'];
+    $order_infos = array_filter($order_infos, function ($order) use ($search_order_id) {
+        return stripos($order['order_id'], $search_order_id) !== false;
+    });
 }
 
+// Handle the status update
+if (isset($_POST['update_status_btn'])) {
+    $status_msg =  $obj->updat_order_status($_POST);
+}
+
+if (isset($status_msg)) {
+    echo $status_msg;
+}
 
 ?>
+
+<div>
+    <form action="manage_order.php" method="POST">
+        <input type="text" name="search_query" placeholder="Search by Customer Mobile">
+        <input type="submit" value="Search" name="search_mobile_btn">
+    </form>
+    <form action="manage_order.php" method="POST">
+        <input type="text" name="search_order_id" placeholder="Search by Order ID">
+        <input type="submit" value="Search" name="search_order_id_btn">
+    </form>
+</div>
+
 <div>
     <table class="table table-striped">
         <thead>
@@ -33,7 +60,7 @@ if(isset( $status_msg)){
                 <th class="product-subtotal">Customer Name</th>
                 <th class="product-subtotal">Customer Mobile</th>
                 <th class="product-subtotal">Trans No</th>
-                <th class="product-subtotal">Shiping</th>
+                <th class="product-subtotal">Shipping</th>
                 <th class="product-subtotal">Order Status</th>
                 <th class="product-subtotal">Update Status</th>
                 <th class="product-subtotal">Placing Time</th>
@@ -41,49 +68,46 @@ if(isset( $status_msg)){
         </thead>
 
         <tbody>
+            <?php
+            foreach ($order_infos as $order_info) {
+            ?>
+                <tr>
+                    <td class="product-name"><?php echo $order_info['order_id'] ?></td>
+                    <td class="product-price"><?php echo $order_info['product_name'] ?></td>
+                    <td class="product-quantity"><?php echo $order_info['pdt_quantity'] ?></td>
+                    <td class="product-subtotal"><?php echo $order_info['amount'] ?></td>
+                    <td class="product-subtotal"><?php echo $order_info['uses_coupon'] ?></td>
+                    <td class="product-subtotal"><?php echo $order_info['customer_name'] ?></td>
+                    <td class="product-subtotal"><?php echo $order_info['Shipping_mobile'] ?></td>
+                    <td class="product-subtotal"><?php echo $order_info['trans_id'] ?></td>
+                    <td class="product-subtotal"><?php echo $order_info['shiping_address'] ?></td>
+                    <td class="product-subtotal">
+                        <?php
+                        if ($order_info['order_status'] == 0) {
+                            echo "<p class='btn btn-danger btn-sm'> Pending </p>";
+                        } elseif ($order_info['order_status'] == 1) {
+                            echo "<p class='btn btn-warning btn-sm'> Processing </p>";
+                        } elseif ($order_info['order_status'] == 2) {
+                            echo "<p class='btn btn-success btn-sm'> Delivered </p>";
+                        }
+                        ?>
+                    </td>
 
-        <?php 
-                foreach($order_infos as $order_info){
-        ?>
-        <tr>
-                <td class="product-name"><?php echo $order_info['order_id'] ?></td>
-                <td class="product-price"><?php echo $order_info['product_name'] ?></td>
-                <td class="product-quantity"><?php echo $order_info['pdt_quantity'] ?></td>
-                <td class="product-subtotal"><?php echo $order_info['amount'] ?></td>
-                <td class="product-subtotal"><?php echo $order_info['uses_coupon'] ?></td>
-                <td class="product-subtotal"><?php echo $order_info['customer_name'] ?></td>
-                <td class="product-subtotal"><?php echo $order_info['Shipping_mobile'] ?></td>
-                <td class="product-subtotal"><?php echo $order_info['trans_id'] ?></td>
-                <td class="product-subtotal"><?php echo $order_info['shiping_address'] ?></td>
-                <td class="product-subtotal">
-                <?php 
-                    if($order_info['order_status']==0){
-                        echo "<p class='btn btn-danger btn-sm'> Pending </p>";
-                    } elseif($order_info['order_status']==1){
-                        echo "<p class='btn btn-warning btn-sm'> Processing </p>";
-                    } elseif($order_info['order_status']==2){
-                        echo "<p class='btn btn-success btn-sm'> Delivered </p>";
-                    }
-                ?>
-                </td>
-
-                <td class="product-subtotal">
-                    <form action="manage_order.php" method="POST">
-                        <select name="update_status">
-                        <option>Select</option>
-                            <option value="0">Pending</option>
-                            <option value="1">Processing</option>
-                            <option value="2">Deliverd</option>
-                        </select> <br>
-                        <input type="hidden" name="order_id" value="<?php echo $order_info['order_id']  ?>">
-                        <input type="submit" value="update" name="update_status_btn">
-                    </form>
-
-                </td>
-                <td class="product-subtotal"><?php echo $order_info['order_time'] ?></td>
-            </tr>
-
-                <?php }?>
+                    <td class="product-subtotal">
+                        <form action="manage_order.php" method="POST">
+                            <select name="update_status">
+                                <option>Select</option>
+                                <option value="0">Pending</option>
+                                <option value="1">Processing</option>
+                                <option value="2">Delivered</option>
+                            </select> <br>
+                            <input type="hidden" name="order_id" value="<?php echo $order_info['order_id'] ?>">
+                            <input type="submit" value="update" name="update_status_btn">
+                        </form>
+                    </td>
+                    <td class="product-subtotal"><?php echo $order_info['order_time'] ?></td>
+                </tr>
+            <?php } ?>
         </tbody>
     </table>
 </div>
--- admin/views/manage_product_view.php
+++ admin/views/manage_product_view.php
@@ -1,22 +1,34 @@
+<?php
+$obj = new adminback();
 
+// Handle the search query
+$search_query = "";
+if (isset($_GET['search'])) {
+    $search_query = $_GET['search'];
+    $product_info = $obj->search_product($search_query);
+} else {
+    $product_info = $obj->display_product();
+}
 
-<?php 
-    $obj=new adminback();
-   $product_info =  $obj->display_product();
-
-   if(isset($_GET['prostatus'])){
-       $id = $_GET['id'];
-       if($_GET['prostatus']=='published'){
-        
-           $obj->published_product($id);
-       }elseif($_GET['prostatus']=='unpublished'){
-           $obj->unpublished_product($id);
-       }elseif($_GET['prostatus']=="delete"){
+if (isset($_GET['prostatus'])) {
+    $id = $_GET['id'];
+    if ($_GET['prostatus'] == 'published') {
+        $obj->published_product($id);
+    } elseif ($_GET['prostatus'] == 'unpublished') {
+        $obj->unpublished_product($id);
+    } elseif ($_GET['prostatus'] == "delete") {
         $del_msg = $obj->delete_product($id);
     }
-   }
+}
 ?>
-<h2>Manage Product </h2> 
+<h2>Manage Product </h2>
+<br>
+
+<!-- Search Form -->
+<form method="GET" action="">
+    <input type="text" name="search" placeholder="Search products..." value="<?php echo $search_query; ?>">
+    <button type="submit">Search</button>
+</form>
 <br>
 
 <table class="table table-bordered">
@@ -24,59 +36,45 @@
         <tr>
             <th>Product Name</th>
             <th>Product Price</th>
-         
             <th>Product Image</th>
             <th>Product Status</th>
-            <th>Product Catagory</th>
+            <th>Product Category</th>
             <th>Action</th>
         </tr>
     </thead>
 
     <tbody>
-<?php 
-    if(isset($del_msg)){
-        echo "{$del_msg}";
-    }
+        <?php
+        if (isset($del_msg)) {
+            echo "{$del_msg}";
+        }
 
-?>
-    <?php while($pdt = mysqli_fetch_assoc($product_info)) {?>
-        <tr>
-            <td> <?php echo $pdt['pdt_name'] ?></td>
-            <td> <?php echo $pdt['pdt_price'] ?></td>
-          
-            <td>  <img style="height:60px" src="uploads/<?php echo $pdt['pdt_img'] ?>" alt=""> </td>
-            <td> 
-                <?php 
-                    if($pdt['pdt_status']==0){
-                        echo "Unpublished";
-                        
-                        ?>
-                        <a href="?prostatus=published&&id=<?php echo $pdt['pdt_id']?>"  class="btn btn-sm btn-primary" >Published</a>
+        ?>
+        <?php while ($pdt = mysqli_fetch_assoc($product_info)) { ?>
+            <tr>
+                <td> <?php echo $pdt['pdt_name'] ?></td>
+                <td> <?php echo $pdt['pdt_price'] ?></td>
+                <td> <img style="height:60px" src="uploads/<?php echo $pdt['pdt_img'] ?>" alt=""> </td>
+                <td>
                     <?php
-                    }else{
+                    if ($pdt['pdt_status'] == 0) {
+                        echo "Unpublished";
+                    ?>
+                        <a href="?prostatus=published&&id=<?php echo $pdt['pdt_id'] ?>" class="btn btn-sm btn-primary">Published</a>
+                    <?php
+                    } else {
                         echo "Published";
-                       
-                         ?>
-                            <a href="?prostatus=unpublished&&id=<?php echo $pdt['pdt_id'] ?>" class="btn btn-sm btn-warning">unpublished</a>
-                         <?php
+                    ?>
+                        <a href="?prostatus=unpublished&&id=<?php echo $pdt['pdt_id'] ?>" class="btn btn-sm btn-warning">Unpublished</a>
+                    <?php
                     }
-                ?>
-            </td>
-            <td> <?php echo $pdt['ctg_name'] ?></td>
-            <td>   <a href="edit_product.php?prostatus=edit&&id=<?php echo $pdt['pdt_id'] ?>">Edit</a> <br>
-             <a href="?prostatus=delete&&id=<?php echo $pdt['pdt_id'] ?>">Delete</a>  </td>
-
-        </tr>
-        <?php }?>
-
-        
+                    ?>
+                </td>
+                <td> <?php echo $pdt['ctg_name'] ?></td>
+                <td> <a href="edit_product.php?prostatus=edit&&id=<?php echo $pdt['pdt_id'] ?>">Edit</a> <br>
+                    <a href="?prostatus=delete&&id=<?php echo $pdt['pdt_id'] ?>">Delete</a>
+                </td>
+            </tr>
+        <?php } ?>
     </tbody>
 </table>
-
-
-<script>
-
-
-
-
-</script>
